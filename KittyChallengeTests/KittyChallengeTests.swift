f4012cfca3c65714953936a11d04f629b5aa4284
jeff-tabios <jefftabios1@gmail.com>
2019-07-22 08:37:00 +0800

    Initial project files

---
+++ KittyChallengeTests/KittyChallengeTests.swift
@@ -0,0 +1,16 @@
+//
+//  KittyChallengeTests.swift
+//  KittyChallengeTests
+//
+//  Created by Jeff Tabios on 22/07/2019.
+//  Copyright © 2019 Jeff Tabios. All rights reserved.
+//
+
+import XCTest
+@testable import KittyChallenge
+
+class KittyChallengeTests: XCTestCase {
+
+
+
+}
